6b1dc44cd55bc8f4e86e683a60efa2dbd90b1128
Garrett Rowell <garrett@puppet.com>
2022-12-02 12:30:28 -0500

    add codemanager

--- lib/puppet/application/visual.rb
+++ lib/puppet/application/visual.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 require 'puppet/util/nc_classifier'
+require 'puppet/util/nc_codemanager'
+
 require 'puppet/util/cli-tree'
 
 class Puppet::Application::Visual < Puppet::Application
@@ -39,6 +41,10 @@ ACTIONS
 
 * all_groups:
   Query the classification endpoint and print out a tree view of all groups
+
+* deploy_status:
+  Query the deploy-status endpoint and print out pretty JSON
+
     HELP
   end
 
@@ -47,27 +53,37 @@ ACTIONS
     send(@command_line.args.shift)
   end
 
+  option("--auth_token [TOKEN]") do |v|
+    options[:auth_token] = v
+  end
+
   def environment_groups
-    classifier = Puppet::Util::Nc_classifier.new
+    classifier = Puppet::Util::Nc_classifier.new(options: options)
     my_json = classifier.get('/classifier-api/v1/groups')
     print_classifier_groups(my_json,'env_gp')
   end
 
   def node_groups
-    classifier = Puppet::Util::Nc_classifier.new
+    classifier = Puppet::Util::Nc_classifier.new(options: options)
     my_json = classifier.get('/classifier-api/v1/groups')
     print_classifier_groups(my_json,'n_gp')
   end
 
   def all_groups
-    classifier = Puppet::Util::Nc_classifier.new
+    classifier = Puppet::Util::Nc_classifier.new(options: options)
     my_json = classifier.get('/classifier-api/v1/groups')
     print_classifier_groups(my_json)
   end
 
+  def deploy_status
+    codemanager = Puppet::Util::Nc_codemanager.new(options: options)
+    my_json = codemanager.get('/code-manager/v1/deploys/status')
+    puts JSON.pretty_generate(my_json)
+  end
+
   def print_classifier_groups(json_data, filter = nil)
     groups = {}
-    
+
     # json looking for env groups
     json_data.each do |group|
 
@@ -77,21 +93,20 @@ ACTIONS
       when 'n_gp'
         next if group['environment_trumps']
       end
-    
+
       groups[group['id']] = {
         'environment_trumps' => group['environment_trumps'],
         'name'               => group['name'],
         'parent'             => group['parent'],
         'environment'        => group['environment']
       }
-    
     end
-    
+
     # make sure we have the parent groups
     missing = {}
     groups.each do |_id, data|
       next if groups.key?(data['parent'])
-    
+
       missing[data['parent']] = {
         'environment_trumps' => json_data[data['parent'].to_i]['environment_trumps'],
         'name'               => json_data[data['parent'].to_i]['name'],
@@ -99,15 +114,15 @@ ACTIONS
         'environment'        => json_data[data['parent'].to_i]['environment']
       }
     end
-    
+
     groups.merge!(missing)
-    
+
     # Find the root of the tree
     top = groups.select { |id, data| id == data['parent'] }.keys.first
-    
+
     # Used to store children and id's
     tree = Hash.new { |h, k| h[k] = { :id => nil, :name => nil, :children => [ ] } }
-    
+
     # Find all the children and keep track
     groups.each do |id, data|
       eg = groups[id]['environment_trumps']
@@ -116,7 +131,7 @@ ACTIONS
       tree[id][:name] = eg ? "#{data['name']} (Env: #{data['environment']})" : "#{data['name']}"
       tree[parent][:children].push(tree[id])
     end
-    
+
     # Print out
     Puppet::Util::TreeNode.from_h(tree[top]).print
 
--- lib/puppet/util/nc_codemanager.rb
+++ lib/puppet/util/nc_codemanager.rb
@@ -1,22 +1,24 @@
 require 'faraday'
 require 'faraday_middleware'
 
-class Puppet::Util::Nc_classifier(options:)
-  def initialize
-    classification_server = Puppet.settings['server']
-    classification_port   = 4433
-    classification_url   = "https://#{classification_server}:#{classification_port}"
+class Puppet::Util::Nc_codemanager
+  def initialize(options:)
+    server = Puppet.settings['server']
+    code_manager_port = 8170
+    code_manager_url  = "https://#{server}:#{code_manager_port}"
 
     @conn = Faraday.new(
-      url: classification_url,
-      headers: { 'Content-Type' => 'application/json' },
+      url: code_manager_url,
+      headers: {
+        'Content-Type'     => 'application/json',
+        'X-Authentication' => options[:auth_token]
+      },
       ssl: {
         client_cert: OpenSSL::X509::Certificate.new(File.read Puppet.settings['hostcert']),
         client_key:  OpenSSL::PKey::RSA.new(File.read Puppet.settings['hostprivkey']),
         ca_file:     Puppet.settings['localcacert'],
       }
     )
-
   end
 
   def get(endpoint)
